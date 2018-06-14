5b66eec117ccf9b5dc123c220a8e546bd02e7973
Leonardo Bautista Gomez <leonardo.bautista@bsc.es>
2018-06-14 12:47:01 +0200

    More documentation

--- sharding_fork_choice_poc/src/network.py
+++ sharding_fork_choice_poc/src/network.py
@@ -1,9 +1,13 @@
 from tools import transform, normal_distribution
 import random
 
-
+##  This class represents a peer to peer network.
+#   It will be refactored to run in a distributed system.
 class NetworkSimulator():
-
+    ##  This method is the constructor which initializes the network.
+    #   It receives the simulated latency of the network.
+    #   @param self             Pointer to this network.
+    #   @param latency          Network latency, set to 50 by default.
     def __init__(self, latency=50):
         self.agents = []
         self.latency_distribution_sample = transform(normal_distribution(latency, (latency * 2) // 5), lambda x: max(x, 0))
@@ -12,6 +16,10 @@ class NetworkSimulator():
         self.peers = {}
         self.reliability = 0.9
 
+    ##  This method generates the list of peers for each peer.
+    #   The peer selection is done randomly.
+    #   @param self             Pointer to this network.
+    #   @param num_peers        Number of peers for each peer. Set to 5 by default.
     def generate_peers(self, num_peers=5):
         self.peers = {}
         for a in self.agents:
@@ -24,6 +32,8 @@ class NetworkSimulator():
             for peer in p:
                 self.peers[peer.id] = self.peers.get(peer.id, []) + [a]
 
+    ##  This method simulates a time tick in the network.
+    #   @param self             Pointer to this network.
     def tick(self):
         if self.time in self.objqueue:
             for recipient, obj in self.objqueue[self.time]:
@@ -34,10 +44,16 @@ class NetworkSimulator():
             a.tick()
         self.time += 1
 
+    ##  This method runs the simulattion in the network.
+    #   @param self             Pointer to this network.
     def run(self, steps):
         for i in range(steps):
             self.tick()
 
+    ##  This method broadcast an object to the network.
+    #   @param self             Pointer to this network.
+    #   @param sender           Pointer to the sender.
+    #   @param obj              Pointer to the object to be broadcasted.
     def broadcast(self, sender, obj):
         for p in self.peers[sender.id]:
             recv_time = self.time + self.latency_distribution_sample()
@@ -45,6 +61,11 @@ class NetworkSimulator():
                 self.objqueue[recv_time] = []
             self.objqueue[recv_time].append((p, obj))
 
+    ##  This method sends an object directly to another peer in the network.
+    #   @param self             Pointer to this network.
+    #   @param sender           Pointer to the sender.
+    #   @param to_id            ID of the target.
+    #   @param obj              Pointer to the object to be broadcasted.
     def direct_send(self, to_id, obj):
         for a in self.agents:
             if a.id == to_id:
@@ -53,6 +74,9 @@ class NetworkSimulator():
                     self.objqueue[recv_time] = []
                 self.objqueue[recv_time].append((a, obj))
 
+    ##  This method knocks offline several nodes in the network.
+    #   @param self             Pointer to this network.
+    #   @param n                Number of nodes to be knocked offline.
     def knock_offline_random(self, n):
         ko = {}
         while len(ko) < n:
@@ -63,6 +87,8 @@ class NetworkSimulator():
         for a in self.agents:
             self.peers[a.id] = [x for x in self.peers[a.id] if x.id not in ko]
 
+    ##  This method creates a partition in the network.
+    #   @param self             Pointer to this network.
     def partition(self):
         a = {}
         while len(a) < len(self.agents) / 2:
@@ -73,3 +99,4 @@ class NetworkSimulator():
                 self.peers[c.id] = [x for x in self.peers[c.id] if x.id in a]
             else:
                 self.peers[c.id] = [x for x in self.peers[c.id] if x.id not in a]
+
--- sharding_fork_choice_poc/src/node.py
+++ sharding_fork_choice_poc/src/node.py
@@ -2,9 +2,27 @@ from block import BeaconBlock, MainChainBlock, ShardCollation, BlockMakingReques
 from tools import to_hex, checkpow
 import random
 
+##  This class represents a node in the network.
+#   It will be refactored into a distributed version.
 class Node():
-
+    ##  This method is the constructor which initializes the node.
+    #   It receives the genesis block from the main chain and the beacon. It
+    #   also receives the number of shards and notaries in the network in
+    #   addition to other few parameters.
+    #   @param self             Pointer to this node.
+    #   @param id               Network ID.
+    #   @param nb_shards        Number of shards.
+    #   @param nb_notaries      Number of notaries in the network.
+    #   @param powdiff          TBD.
+    #   @param main_genesis     Genesis block in the main chain.
+    #   @param beacon_genesis   Genesis block in the beacon chain.
+    #   @param shard_genesis    Genesis block in the shard.
+    #   @param sleepy           Set to True if the node is sleepy.
+    #   @param careless         Set to True if the node is careless.
+    #   @param base_ts_diff     TBD.
+    #   @param skip_ts_diff     TBD.
     def __init__(self, _id, network, nb_shards, nb_notaries, powdiff, main_genesis, beacon_genesis, shard_geneses, sleepy=False, careless=False, base_ts_diff=1, skip_ts_diff=6):
+    genesis, shard_geneses, sleepy=False, careless=False, base_ts_diff=1, skip_ts_diff=6):
         self.blocks = {
             beacon_genesis.hash: beacon_genesis,
             main_genesis.hash: main_genesis
